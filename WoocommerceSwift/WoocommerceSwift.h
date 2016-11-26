e3b0256315a5941d21cf6a9f9c700c574bb80d37
Dmitri Kalinaitsev <dkalinai@microsoft.com>
2016-11-26 09:25:58 +0200

    - Initial commit

---
+++ WoocommerceSwift/WoocommerceSwift.h
@@ -0,0 +1,19 @@
+//
+//  WoocommerceSwift.h
+//  WoocommerceSwift
+//
+//  Created by Dmitri Kalinaitsev on 26/11/2016.
+//  Copyright © 2016 Dmitri Kalinaitsev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WoocommerceSwift.
+FOUNDATION_EXPORT double WoocommerceSwiftVersionNumber;
+
+//! Project version string for WoocommerceSwift.
+FOUNDATION_EXPORT const unsigned char WoocommerceSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WoocommerceSwift/PublicHeader.h>
+
+
